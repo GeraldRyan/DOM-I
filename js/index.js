5a8906df62e9705cb89b1f43213a36aa177dce34
Webbuildermn <gerald.ryan40@yahoo.com>
2020-03-09 14:55:33 -0500

    First commit, got innerHTML done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,7 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let nav1 = document.querySelector("nav a");
+console.log(nav1)
+nav1.innerHTML =siteContent["nav"]["nav-item-1"]
